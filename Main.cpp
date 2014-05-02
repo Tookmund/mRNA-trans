d036d2c12c20fd87b6b82d698997d326ae2deacd
Jacob Adams <jacobaa1031@gmail.com>
2014-05-02 10:21:43 -0400

    ASCII interface and STDIN input

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include "Translator.h"
 
-int main(int argc, char* argv[]){
+std::string menu = 
+"+-------------------+\n"
+"|  mRNA Translator  |\n"
+"|[1] Read from File |\n"
+"|[2] Read from STDIN|\n"
+"|[3] Quit           |\n"
+"+-------------------+\n";
 
-	std::cout << "Please Enter input filename: \n";
+Translator inputFile () {
+std::cout << "Please Enter input filename: \n";
 	
 	std::string filename;
 	std::cin >> filename;
@@ -14,6 +21,36 @@ int main(int argc, char* argv[]){
 		return 0;
 
 	translator.translate();
+	return translator;
+}
+
+Translator STDIN() {
+	std::string mrna;
+	std::cout << "Please input mRNA codons:";
+	std::cin >> mrna;
+	Translator translator;
+	translator.loadmRNA(mrna);
+	return translator;
+}
+
+int main(int argc, char* argv[]){
+	std::cout << menu;
+	int choice;
+	Tranlator translator;
+	std::cin >> choice;
+	switch(choice) {
+		case 1:
+			translator = inputFile();
+			break;
+		case 2:
+			translator = STDIN();
+			break;
+		case 3:
+		default:
+			return 0;
+			break;
+	}
+	Translator translator 
 
 	std::cout << "Please Enter Output filename: \n";
 	
